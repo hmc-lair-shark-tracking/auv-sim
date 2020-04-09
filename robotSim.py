fc82e1a40d03dff5c2ec4fa0c56cff37dd939596
Yuki Wang <yukiluna1999@gmail.com>
2020-04-08 23:23:55 -0700

    rename and add stubs for all functions

--- robotSim.py
+++ robotSim.py
@@ -1,13 +1,30 @@
 import math
 import time  # temporarily import time so the code can sleep
 
-class Robot:
+class RobotSim:
     def __init__(self, initX, initY, initTheta):
         self.x = initX
         self.y = initY
         self.theta = initTheta
 
-    def getSharkState(self, v, w, delta_t):
+    def getAuvState(self):
+        """
+        Return a tuple representing the orientation
+        of the robot
+        """
+        return (self.x, self.y, self.theta)
+
+    def getSharkState(self):
+        """
+        Return a tuple representing the orientation
+        of the shark
+        """
+        sharkX = 10
+        sharkY = 10
+        sharkTheta = 0
+        return (sharkX, sharkY, sharkTheta)
+
+    def calculateNewAuvState (self, v, w, delta_t):
         """ 
         Calculate new x, y and theta
 
@@ -20,6 +37,24 @@ class Robot:
         self.y = self.y + v * math.sin(self.theta)*delta_t
         self.theta = self.theta + w * delta_t
 
+    def sendTrajectoryToActuators(self, v, w):
+        # TODO: For now this should just update AUV States?
+
+        # dummy value: set time step
+        delta_t = 0.1
+        self.calculateNewAuvState(v, w, delta_t)
+
+    def logData(self):
+        """
+        Print in the terminal (and possibly write the 
+        data in a log file)
+        """
+    
+    def plotData(self):
+        """
+        Plot the position of the robot and the shark
+        """
+
     def mainNavigationLoop(self):
         """ 
         Wrapper function for the robot simulator
@@ -31,7 +66,8 @@ class Robot:
             # dummy values for linear and angular velocity
             v = 10
             w = 10
-            self.getSharkState(v, w, 0.1)
+            self.sendTrajectoryToActuators(v, w)
+            
             print("x postion: ", self.x)
             print("y position: ", self.y)
             print("theta: ", self.theta)
@@ -39,7 +75,7 @@ class Robot:
             time.sleep(1)
 
 def main():
-    testRobot = Robot(0,0,0.1)
+    testRobot = RobotSim(0,0,0.1)
     testRobot.mainNavigationLoop()
 
 if __name__ == "__main__":
