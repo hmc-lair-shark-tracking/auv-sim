ed4ac66ff4fddc9e669cbb630cca8a7b396280fc
Yuki Wang <yukiluna1999@gmail.com>
2020-04-09 17:51:09 -0700

    able to connect scatter points in 3D plots

--- robotSim.py
+++ robotSim.py
@@ -1,18 +1,29 @@
 import math
 import matplotlib.pyplot as plt
 import time  # temporarily import time so the code can sleep
-
+from mpl_toolkits.mplot3d import Axes3D
 
 class RobotSim:
-    def __init__(self, initX, initY, initTheta):
+    def __init__(self, initX, initY, initZ, initTheta):
         # initialize auv's data
         self.x = initX
         self.y = initY
         self.theta = initTheta
 
+        # need lists to keep track of all the points if we want to
+        #   connect lines between the position points and create trajectories
+        self.xList = [initX]
+        self.yList = [initY]
+        self.zList = [initZ]
+
+        self.sharkXList = []
+        self.sharkYList = []
+        self.sharkZList = []
+
         # initialize the 3d scatter position plot for the auv and shark
         self.fig = plt.figure()
         self.ax = self.fig.add_subplot(111, projection='3d')
+
 
     def getAuvState(self):
         """
@@ -21,6 +32,7 @@ class RobotSim:
         """
         return (self.x, self.y, self.theta)
 
+
     def getSharkState(self):
         """
         Return a tuple representing the orientation
@@ -28,8 +40,15 @@ class RobotSim:
         """
         sharkX = 10
         sharkY = 10
+        sharkZ = -15
         sharkTheta = 0
+
+        self.sharkXList += [sharkX]
+        self.sharkYList += [sharkY]
+        self.sharkZList += [sharkZ]
+
         return (sharkX, sharkY, sharkTheta)
+
 
     def calculateNewAuvState (self, v, w, delta_t):
         """ 
@@ -44,6 +63,11 @@ class RobotSim:
         self.y = self.y + v * math.sin(self.theta)*delta_t
         self.theta = self.theta + w * delta_t
 
+        self.xList += [self.x]
+        self.yList += [self.y]
+        self.zList += [self.zList[0]]
+
+
     def sendTrajectoryToActuators(self, v, w):
         # TODO: For now this should just update AUV States?
 
@@ -57,11 +81,11 @@ class RobotSim:
         Print in the terminal (and possibly write the 
         data in a log file?)
         """
+
         print("AUV [x postion, y position, theta]:  [", self.x, ", " , self.y, ", ", self.theta, "]")
 
-        (sharkX, sharkY, sharkTheta)= self.getSharkState()
-
-        print("Shark [x postion, y position, theta]:  [", sharkX, ", " , sharkY, ", ", sharkTheta, "]")
+        # get the latest position from the shark postion lists
+        print("Shark [x postion, y position, theta]:  [", self.sharkXList[-1], ", " , self.sharkYList[-1], ", ", self.sharkZList[-1], "]")
     
     def plotData(self):
         """
@@ -69,11 +93,15 @@ class RobotSim:
         """
         # plot the new auv position as a red "o"
         self.ax.scatter(self.x, self.y, -10, marker = "o", color='red')
-
-        (sharkX, sharkY, sharkTheta)= self.getSharkState()
+        # draw the lines between the points
+        self.ax.plot(self.xList, self.yList, self.zList, color='red')
+      
      
         # plot the new shark position as a blue "o"
-        self.ax.scatter(sharkX, sharkY, -15, marker = "x", color="blue")
+        # get the latest position from the shark postion lists
+        self.ax.scatter(self.sharkXList[-1], self.sharkYList[-1], self.sharkZList[-1], marker = "x", color="blue")
+        # draw the lines between the points
+        self.ax.plot(self.sharkXList, self.sharkYList, self.sharkZList, color='red')
 
         plt.draw()
 
@@ -93,6 +121,14 @@ class RobotSim:
             # dummy values for linear and angular velocity
             v = 10
             w = 10
+            
+            (currAuvX, currAuvY, currAuvTheta) = self.getAuvState()
+            print("==================")
+            print("Testing getAuvState [x, y, theta]:  [", currAuvX, ", " , currAuvY, ", ", currAuvTheta, "]")
+            
+            (currSharkX, currSharkY, currSharkTheta) = self.getSharkState()
+            print("Testing getSharkState [x, y, theta]:  [", currSharkX, ", " , currSharkY, ", ", currSharkTheta, "]")
+            print("==================")
 
             # update the auv position
             self.sendTrajectoryToActuators(v, w)
@@ -102,7 +138,7 @@ class RobotSim:
             self.plotData()
 
 def main():
-    testRobot = RobotSim(10,10,0.1)
+    testRobot = RobotSim(10,10,-10,0.1)
     testRobot.mainNavigationLoop()
 
 if __name__ == "__main__":
