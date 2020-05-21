c94987e084cc64ddb2d133acb837b862d286cb95
Yuki Wang <yukiluna1999@gmail.com>
2020-05-21 09:39:14 -0700

    tried pynput library but seems to harm the main thread?

--- robotSim.py
+++ robotSim.py
@@ -2,8 +2,8 @@ import math
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-
 import csv
+
 # import 3 data representation class
 from objectState import ObjectState
 from sharkState import SharkState
