54aca9cbecbbf1e6bf2234a5c7efc03d532fa13f
Yuki Wang <yukiluna1999@gmail.com>
2020-06-29 13:57:23 -0700

    DQN Discrete Habitat Space - rewrite how the number of time visited is stored

--- habitatGrid.py
+++ habitatGrid.py
@@ -2,6 +2,9 @@ from habitatState import HabitatState
 
 """
 A wrapper class to discretize the environment into grids
+
+Maybe to update the number of time visited, we have an extra array storing that in that class. Habitat State will be store the index into 
+that arrray instead. Then, we can increment the number of time that way. In this sense, we will be able to have different shape in the future?
 """
 
 class HabitatGrid:
@@ -16,9 +19,12 @@ class HabitatGrid:
         row_hab_id = 0
         col_hab_id = 0
 
+        self.habitat_grid = []
+
         for row in range(self.env_size_y // self.cell_side_length):
             self.habitat_grid.append([])
             if row % self.habitat_side_length == 0 and row != 0:
+                # inside a new habitat
                 row_hab_id = col_hab_id + 1
 
             col_hab_id = row_hab_id
@@ -29,7 +35,11 @@ class HabitatGrid:
                     col_hab_id += 1
                 hab_x = env_x + col * cell_side_length
                 hab_y = env_y + row * cell_side_length
-                self.habitat_grid[row].append(HabitatState(hab_x, hab_y, col_hab_id))
+                self.habitat_grid[row].append(HabitatState(hab_x, hab_y, col_hab_id, side_length = self.cell_side_length))
+
+        number_of_habitats = self.habitat_grid[-1][-1].habitat_id + 1
+
+        self.habitat_num_of_time_visited = [0] * number_of_habitats
 
 
     def print_habitat_grid(self):
@@ -56,8 +66,8 @@ class HabitatGrid:
         auv_x = auv_pos[0]
         auv_y = auv_pos[1]
 
-        hab_index_row = int(auv_y)
-        hab_index_col = int(auv_x)
+        hab_index_row = int(auv_y / self.cell_side_length)
+        hab_index_col = int(auv_x / self.cell_side_length)
 
         if hab_index_row >= len(self.habitat_grid):
             print("auv is out of the habitat environment bound verticaly")
@@ -75,4 +85,7 @@ def main():
 
     testing_grid.print_habitat_grid()
 
-                
+    auv_pos = [2, 1]
+    testing_grid.inside_habitat(auv_pos)
+
+
--- habitatState.py
+++ habitatState.py
@@ -3,17 +3,16 @@ A wrapper class to represent state of an habitat
     including x, y, z, size, and the number of time visited
 """
 class HabitatState:
-    def __init__(self, x, y, habitat_id, side_length = 1, num_of_time_visited = 0):
+    def __init__(self, x, y, habitat_id, side_length = 1):
         self.x = x
         self.y = y
         self.side_length = side_length
         self.habitat_id = habitat_id
-        self.num_of_time_visited = 0
 
     def __repr__(self):
         return "Habitat: [id=" + str(self.habitat_id) + ", x=" + str(self.x) + ", y="  + str(self.y) + ", side length=" +\
-            str(self.side_length)  + ", visited=" +  str(self.num_of_time_visited) + "]"
+            str(self.side_length) + "]"
 
     def __str__(self):
         return "Habitat: [id=" + str(self.habitat_id) + ", x=" + str(self.x) + ", y="  + str(self.y) + ", side length=" +\
-            str(self.side_length)  + ", visited=" +  str(self.num_of_time_visited) + "]"
+            str(self.side_length) + "]"
